f273b3474c870aaefea4fae28d8f672e2e327c61
JoseFernando7 <jose.f72000@gmail.com>
2025-08-05 09:12:22 -0500

    Linter configured

--- src/users/application/UserCreate/user.create.ts
+++ src/users/application/UserCreate/user.create.ts
@@ -1,20 +1,24 @@
-import { UserRepository } from "src/users/domain/user.repository";
-import { UserId } from "src/users/domain/user.id";
-import { UserCreatedAt } from "src/users/domain/user.created-at";
-import { User } from "src/users/domain/user";
+import { UserRepository } from 'src/users/domain/user.repository';
+import { UserId } from 'src/users/domain/user.id';
+import { UserCreatedAt } from 'src/users/domain/user.created-at';
+import { User } from 'src/users/domain/user';
 
 export class UserCreate {
-  constructor(private readonly userRespository: UserRepository) { }
+  constructor(private readonly userRespository: UserRepository) {}
 
-  async run(id: string, name: string, email: string, createdAt: Date): Promise<void> {
+  async run(
+    id: string,
+    name: string,
+    email: string,
+    createdAt: Date,
+  ): Promise<void> {
     const user = new User(
       new UserId(id),
       name,
       email,
-      new UserCreatedAt(createdAt)
+      new UserCreatedAt(createdAt),
     );
 
     return this.userRespository.create(user);
   }
 }
-
